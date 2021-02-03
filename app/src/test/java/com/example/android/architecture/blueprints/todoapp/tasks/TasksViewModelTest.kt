5bce559d162c571a4eb22ef9f050544dfef53355
Filipe Bezerra <filipebzerra@gmail.com>
2021-02-03 11:02:34 -0300

    refactor: Get rid of boilerplate code for observing the LiveData

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,16 +1,14 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.Observer
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.android.architecture.blueprints.todoapp.Event
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import org.hamcrest.CoreMatchers.not
 import org.hamcrest.CoreMatchers.nullValue
-import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.Assert.assertThat
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -21,17 +19,14 @@ class TasksViewModelTest {
 
     @Test
     fun given_user_when_clicked_on_fab_then_navigate_to_add_edit_task_screen() {
+        // Given a fresh TasksViewModel
         val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
 
-        val observer = Observer<Event<Unit>> {}
-        try {
-            tasksViewModel.newTaskEvent.observeForever(observer)
-            tasksViewModel.addNewTask()
+        // When adding a new Task
+        tasksViewModel.addNewTask()
 
-            val eventValue = tasksViewModel.newTaskEvent.value
-            assertThat(eventValue?.getContentIfNotHandled(), not(nullValue()))
-        } finally {
-            tasksViewModel.newTaskEvent.removeObserver(observer)
-        }
+        // Then the new task event is triggered
+        val eventValue = tasksViewModel.newTaskEvent.getOrAwaitValue()
+        assertThat(eventValue.getContentIfNotHandled(), not(nullValue()))
     }
 }
